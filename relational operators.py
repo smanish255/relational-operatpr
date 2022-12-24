b9877c97264a2f37f8465297282734bc870f61f5
smanish255 <103683199+smanish255@users.noreply.github.com>
2022-12-24 09:55:55 +0530

    Add files via upload

---
+++ relational operators.py
@@ -0,0 +1,19 @@
+a=float(input("enter an a number: "))
+b=float(input("enter an b number: "))
+print("*"*100)
+print("result of relational operators")
+print("\t {}>{}={}".format(a,b,a>b))
+print("*"*100)
+print("\t {}<{}={}".format(a,b,a<b))
+print("*"*100)
+print("\t {}=={}={}".format(a,b,a==b))
+print("*"*100)
+print("\t {}!={}={}".format(a,b,a!=b))
+print("*"*100)
+print("\t {}>={}={}".format(a,b,a>=b))
+print("*"*100)
+print("\t {}<={}={}".format(a,b,a<=b))
+print("*"*100)
+
+
+            
